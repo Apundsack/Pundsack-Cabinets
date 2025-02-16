73eb11457a31913a2fe58387d2f0b21be4e48b61
austinpundsack <austin.pundsack@ndsu.edu>
2025-02-15 21:53:04 -0600

    changing port to 443

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const fs = require('fs');
 const galleryRoutes = require('./routes/gallery'); // Import gallery routes
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 443;
 
 // Set EJS as the templating engine
 app.set('view engine', 'ejs');
