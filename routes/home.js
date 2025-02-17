09327cf02aa731fd1076e84ed04327f34906f467
austinpundsack <austin.pundsack@ndsu.edu>
2025-02-16 23:23:55 -0600

    styling changes and routing background image for homepage

--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
 router.get('/', (req, res) => {
   res.render('index');
 });
